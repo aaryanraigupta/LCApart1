fedf2cbb676cdd96ed62a7ab411f821f4731044d
Aaryan Rai Gupta <79060113+aaryanraigupta@users.noreply.github.com>
2021-10-14 18:01:57 +0100

    Add files via upload

---
+++ LCATEST.java
@@ -0,0 +1,44 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class lcaTest {
+
+	@Test
+	void test_basic() {
+		LCA tree = new LCA(); 
+        tree.root = new Node(1); 
+        tree.root.left = new Node(2); 
+        tree.root.right = new Node(3); 
+        tree.root.left.left = new Node(4); 
+        tree.root.left.right = new Node(5); 
+        tree.root.right.left = new Node(6); 
+        tree.root.right.right = new Node(7); 
+        
+        assert(tree.findLCA(4,5) == 2);
+	}
+
+	 @Test
+	void test_null() {
+		LCA tree = new LCA();
+		tree.root = null;
+		 
+		 assert(tree.findLCA(5,6) == -1);
+	}
+	
+	@Test
+	void test_lowest_ancestor() {
+		LCA tree = new LCA();
+		tree.root = new Node(3); 
+	    tree.root.left = new Node(5); 
+	    tree.root.right = new Node(1); 
+	    tree.root.left.left = new Node(6); 
+	    tree.root.left.right = new Node(2); 
+	    tree.root.right.left = new Node(0); 
+	    tree.root.right.right = new Node(8);
+	    tree.root.left.right.left = new Node(7);
+	    tree.root.left.right.right = new Node(4);
+	    
+	    assert(tree.findLCA(5,4) == 5);
+	}
+	
+}
